8d1b4e4fa8f2fd64504ffaf0265f73b92752c340
officialzpb-jpg <officialzpb@gmail.com>
2026-02-27 18:29:49 -0500

    Simplify config for Vercel

--- personaverse-fresh/next.config.js
+++ personaverse-fresh/next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  distDir: '.next',
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
 };
 
 module.exports = nextConfig;
